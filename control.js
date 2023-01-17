925c7d69f2f911434dd87501bfdb5ff16b511981
thien0709 <thien070904@gmail.com>
2023-01-17 17:44:42 +0700

    first commit

---
+++ control.js
@@ -0,0 +1,9 @@
+document.querySelector(".btnlearnmore").onclick = () =>{
+    document.querySelector(".timeline").scrollIntoView();
+}
+document.querySelector(".content1").onclick = () =>{
+    document.querySelector(".timeline ul li:nth-child(1)").scrollIntoView();
+}
+document.querySelector(".content2").onclick = () =>{
+    document.querySelector(".timeline ul li:nth-child(8)").scrollIntoView();
+}
